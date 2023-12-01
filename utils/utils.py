cf8d3eeb3a12f6216c28cac58a0f5528626fe22e
Brendow Souza <brendow.souza@sapiensia.com>
2023-12-01 18:00:10 -0300

    main - Handling exceptions of converge_matrix method and adpting program to it.

--- utils/utils.py
+++ utils/utils.py
@@ -8,7 +8,11 @@ def convergeMatrix(matrix, matrixValue):
     if matrixValue != None:
         matrixValueCopy = matrixValue
 
-    #Pegando o tamanho da w
+    #Pegando o tamanho da matrix
+
+    if len(matrix) != len(matrix):
+        raise Exception("Tamanho da matriz de equações não corresponde ao tamanho da matriz de resultados das equações.")
+
     matrixLength = len(matrixCopy)
 
     #Convergendo a matrix
@@ -26,8 +30,10 @@ def convergeMatrix(matrix, matrixValue):
 
     for i in range(matrixLength):
         for j in range(matrixLength):
-
+            #Pega o valor da posicao dessa linha presente na diagonal principal
             actualLineValue = matrixCopy[i][j]
+
+            #Faz a soma dos outros valores da linha atual, desconciderando o valor na diagonal principal
             sumOtherValues = sum([x for x in matrixCopy[i] if x != actualLineValue])
 
             if actualLineValue > sumOtherValues:
