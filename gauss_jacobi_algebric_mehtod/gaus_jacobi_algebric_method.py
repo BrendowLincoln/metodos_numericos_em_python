ba5f915bbf88da65eceedb7d919fe6fd4273fcf6
Brendow Lincoln <linconlbrendow@gmail.com>
2024-01-04 18:20:18 -0300

    main - Finalizing code of gauss jacobi algebric method

--- gauss_jacobi_algebric_mehtod/gaus_jacobi_algebric_method.py
+++ gauss_jacobi_algebric_mehtod/gaus_jacobi_algebric_method.py
@@ -1,14 +1,7 @@
 import sys
-import os
-import math
+import copy
 from typing import List
 
-projeto_dir = os.path.dirname(os.path.abspath(__file__))
-projeto_dir = os.path.dirname(projeto_dir)
-
-sys.path.append(projeto_dir)
-
-from utils.utils import convergeMatrix
 
 # Auxiliary class to help structure of code
 class Equation:
@@ -16,64 +9,149 @@ class Equation:
         self.terms = terms
         self.solution = solution
 
-def gaus_jacobi_algebric(matrix, matrixValue, error, times):
+def gauss_jacobi_algebric(matrix, matrixValue, error, times):
 
-    # Definindo criterio de parada de força bruta
+    # Defining the brute force stopping criterion
     BRUTE_FORCE_TIMES = times
 
-    # Fazendo a convergência da matriz
+    # Performing the convergence of the matrix
     converged_matrix = convergeMatrix(matrix, matrixValue)
 
     if converged_matrix != None:
-        # Pegando o tamanho da matrix
+        # Getting the size of the matrix
         matrix_length = len(converged_matrix["matrix"])
 
-        # Inicializando a matrix de incógnitas com 0
-        iteration_variable_values = [0] * matrix_length
+        # Initializing the matrix of unknowns with 0
+        iteration_variable_initial_values = [0] * matrix_length
 
-        # Isolando incognitas
+        # Isolating variables
         iteration_variable_results = {}
 
-        for i in range(matrix_length):
-            # Defining variable name that will be add in result object
-            current_variable_key = f'x{(i + 1)}'
+        # Inicializing de lines result
+        lines = []
+        line = {}
 
-            equation = Equation(converged_matrix["matrix"][i], converged_matrix["matrixValue"][i])
-            iteration_variable_results[current_variable_key] = resolve_current_variables(equation, iteration_variable_values, i)
+        for i in range(BRUTE_FORCE_TIMES):
+            final_results = []
 
-        print(iteration_variable_results)
+            # Setting index of line
+            line.__setitem__("count", (i+1))
 
-        return converged_matrix
-    
+            for j in range(matrix_length):
+                # Defining variable name that will be add in result object
+                current_variable_key = f'x{(j + 1)}'
 
-def resolve_current_variables(equation: Equation, iteration_variable_values: List[int], variable_target_index: int):
+                equation = Equation(converged_matrix["matrix"][j], converged_matrix["matrixValue"][j])
+                iteration_variable_results[current_variable_key] = resolve_current_variables(equation, iteration_variable_initial_values, j)
 
-    # Retrieving term that match to position of current variable
-    match_position_term = round(equation.terms[variable_target_index], 4)
+            for key, value in iteration_variable_results.items():
+                    line.__setitem__(key, value)
+                    final_results.append(value)
+        
+            # Verifing the break condition
+            variable_result_values_list = list(iteration_variable_results.values())
+            is_break_criteria_attempt = False
 
-    # Extracting the remaining terms of the equation to a new list
-    remaining_terms = remove_element_by_index(equation.terms, variable_target_index)
+            for index, value in enumerate(variable_result_values_list):
+                is_break_criteria_attempt = abs((value - iteration_variable_initial_values[index])) <= error
+                
+                if is_break_criteria_attempt == False:
+                    break
 
-    # Inverting the sign of the remaining terms for calculation
-    remaining_terms = list(map(lambda value: value * (-1), remaining_terms))
+            if is_break_criteria_attempt:
+                line.__setitem__("statusMessage", "Finalizar")
+                lines.append(line)
+                result = {"lines": lines, "final_results": final_results}
 
-    # Extracting the iteration variable values that match to the remaining terms position
-    remaining_terms_iteration_variable_values = remove_element_by_index(iteration_variable_values, variable_target_index)
+                return result
+            
+            line.__setitem__("statusMessage", "Continuar")
+            lines.append(copy.deepcopy(line))
 
-    # Starting the calculation by adding the solution of the equation to the initial value of the current variable result 
-    variable_result = round(equation.solution)
-    
-    # Calculating the product for each remaining term with a matching iteration variable value and summing the product to the current iteration variable result
-    for index, equation_value in enumerate(remaining_terms):
-        variable_result += (round(equation_value, 4) * round(remaining_terms_iteration_variable_values[index], 4))
-    
-    # For the final step of calculation, divide the actual value by the term at the matching position
-    variable_result = round((variable_result / match_position_term), 4)
+            for index, value in enumerate(variable_result_values_list):
+                iteration_variable_initial_values[index] = value
 
-    return variable_result
+def resolve_current_variables(equation: Equation, iteration_variable_initial_values: List[int], variable_target_index: int):
 
+    try:
+        # Retrieving term that match to position of current variable
+        match_position_term = round(equation.terms[variable_target_index], 4)
+
+        # Extracting the remaining terms of the equation to a new list
+        remaining_terms = remove_element_by_index(equation.terms, variable_target_index)
+
+        # Inverting the sign of the remaining terms for calculation
+        remaining_terms = list(map(lambda value: value * (-1), remaining_terms))
+
+        # Extracting the iteration variable values that match to the remaining terms position
+        remaining_terms_iteration_variable_values = remove_element_by_index(iteration_variable_initial_values, variable_target_index)
+
+        # Starting the calculation by adding the solution of the equation to the initial value of the current variable result 
+        variable_result = round(equation.solution)
+
+        # Calculating the product for each remaining term with a matching iteration variable value and summing the product to the current iteration variable result
+        for index, equation_value in enumerate(remaining_terms):
+            variable_result += (round(equation_value, 4) * round(remaining_terms_iteration_variable_values[index], 4))
+
+        # For the final step of calculation, divide the actual value by the term at the matching position
+        variable_result = round((variable_result / match_position_term), 4)
+
+        return variable_result
+
+    except:
+        _, message, __ = sys.exc_info()
+        print("".join(["ERROR!: An error has ocurred when resolve current variables calculation: ", str(message), "\n"]))
+        raise
 
 def remove_element_by_index(list: List, index: int):
 
     filtered_list = list[:index] + list[index+1:]
     return filtered_list
+
+def convergeMatrix(matrix, matrixValue):
+    try:
+        #Criando uma copia de matrix
+        matrixCopy = copy.deepcopy(matrix)
+        matrixValueCopy = copy.deepcopy(matrixValue)
+
+        #Pegando o tamanho da matrix
+        if len(matrix) != len(matrixValue):
+            raise Exception("Tamanho da matriz de equações não corresponde ao tamanho da matriz de resultados das equações.")
+
+        matrixLength = len(matrixCopy)
+
+        #Convergendo a matrix
+        if matrixValue != None:
+            convergedMatrix = {
+                "matrix": [],
+                "matrixValue": []
+            }
+        else:
+            convergedMatrix = {
+                "matrix": []
+            }
+
+        biggestLineValueIndex = 0
+
+        for i in range(matrixLength):
+            for j in range(matrixLength):
+                #Pega o valor da posicao dessa linha presente na diagonal principal
+                actualLineValue = matrixCopy[i][j]
+
+                #Faz a soma dos outros valores da linha atual, desconciderando o valor na diagonal principal
+                sumOtherValues = sum([x for x in matrixCopy[i] if x != actualLineValue])
+
+                if actualLineValue > sumOtherValues:
+                    biggestLineValueIndex = j
+                    convergedMatrix["matrix"].insert(biggestLineValueIndex, matrixCopy[i])
+                    if matrixValue != None:
+                        convergedMatrix["matrixValue"].insert(biggestLineValueIndex, matrixValueCopy[i])
+                    break
+
+        # Retorna a matriz convergida 
+        return convergedMatrix
+    except Exception as e:
+        print("Ocorreu um erro!\n")
+        _, message, __ = sys.exc_info()
+        print(message)
+        return
--- gauss_jacobi_algebric_mehtod/main.py
+++ gauss_jacobi_algebric_mehtod/main.py
@@ -6,7 +6,7 @@ projeto_dir = os.path.dirname(projeto_dir)
 
 sys.path.append(projeto_dir)
 
-from gaus_jacobi_algebric_method import gaus_jacobi_algebric
+from gaus_jacobi_algebric_method import gauss_jacobi_algebric
 
 def main():
 
@@ -14,31 +14,60 @@ def main():
 
     # Variaveis 
     matrix = [
-        [1, 8, -1],
-        [6, -1, 1],
-        [1, 1, 5],
+        [10, 1, 1],
+        [1, 5, 9],
+        [2, 8, -4],
     ]
 
-    matrixValue = [16, 7, 18]
+    matrixValue = [12, 15, 6]
 
-    error = 0.01
+    error = 0.02
 
     times = 100
 
-    result = gaus_jacobi_algebric(matrix, matrixValue, error, times)
+    results = gauss_jacobi_algebric(matrix, matrixValue, error, times)
 
-    if result != None:
-        print("Matrix Original\n")
+    if results != None:
+        quantity_variable = len(matrix)
+        
+        string_header = ""
 
-        for index, item in enumerate(matrix):
-            print(item, " = ", matrixValue[index])
+        string_header += "| k \t"
 
+        for i in range(quantity_variable):
+            
+            string_header += ("| " + "x" + str(results["lines"][i]["count"]) + " \t\t")
+
+        string_header += "| Situação \t|"
+
+        print(string_header)
+
+        for item in results["lines"]:
+            string_line = "| "
+            string_line += (str(item["count"]) + " \t|")
+
+            for i in range(quantity_variable):
+                key = f'x{(i + 1)}'
+
+                string_line += (str(item[key]) + "\t\t|")
+
+            string_line += (" " + str(item["statusMessage"]) + " \t|")
+
+            print(string_line)
+        
         print()
 
-        print("Matrix Convergida\n")
 
-        for index, item in enumerate(result["matrix"]):
-            print(item, " = ", result["matrixValue"][index])
+        final_results = "Os valores das variáveis: ("
+        
+        for index, value in enumerate(results["final_results"]):
+            if (index + 1) != quantity_variable:
+                final_results += (str(value) + ", ") 
+            else:
+                final_results += (str(value) + ")")
+        
+        print("\nRESULTADOS\n")
+        print(final_results, "\n")
 
 if __name__ == "__main__":
     main()
